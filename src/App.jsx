2e93bab80237efcb8dde6abebd54523f6d18e090
Steven Egnaczyk <stevenegnaczyk@gmail.com>
2024-10-02 17:25:25 -0400

    got rid of unused import

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Home from './pages/Home';
-import { auth } from './config/firebase';
 
 const App = () => {
   const [user, setUser] = useState(null);
