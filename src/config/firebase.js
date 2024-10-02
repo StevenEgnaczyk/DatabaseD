1c6df80b101eec3edebab174e24cd42a051d53ae
Steven Egnaczyk <stevenegnaczyk@gmail.com>
2024-10-01 20:48:20 -0400

    Added in FireBase authentication to the website - currently allows you to log-in via google.

---
+++ src/config/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAvfUTGT2fa9SvlwW7i8I35-gthphnNz_4",
+  authDomain: "databased-d255f.firebaseapp.com",
+  projectId: "databased-d255f",
+  storageBucket: "databased-d255f.appspot.com",
+  messagingSenderId: "608800540836",
+  appId: "1:608800540836:web:f5f6b9b1aac575ff643e31",
+  measurementId: "G-5C713GHVJE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
