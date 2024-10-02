ee4affe04d6133588ee1ded7ea1c1743658e8b3f
Steven Egnaczyk <stevenegnaczyk@gmail.com>
2024-10-01 21:11:33 -0400

    test

--- scripts/prevent-main-push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-branch="$(git rev-parse --abbrev-ref HEAD)"
-if [ "$branch" = "main" ]; then
-  echo "You are trying to push to the 'main' branch. Please switch to a different branch."
-  exit 1
-fi
