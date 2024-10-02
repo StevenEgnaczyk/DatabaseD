c8c9e56090ed9bafddacae1716c25e2876b96972
Steven Egnaczyk <stevenegnaczyk@gmail.com>
2024-10-02 18:53:45 -0400

    Changed the text

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -20,7 +20,7 @@ const Login = ({ setUser }) => {
 
   return (
       <form onSubmit={handleLogin}>
-        <h2>Login</h2>
+        <h2>Log-in</h2>
         <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required />
         <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
         <button type="submit">Login</button>
