fc24092f03d26cc7dfa5cbba80e17650412476ed
Kellen <kellen.gary@gmail.com>
2024-10-03 15:10:01 -0400

    Unfinished but more homepage styling for Main branch

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,8 @@ import React from 'react';
 import "./Home.css"
 import NavBar from './../../components/Home/NavBar';
 import NavBarLeft from './../../components/Home/NavBarLeft';
+import FileQueryBar from './../../components/Home/FileQueryBar';
+
 
 const Home = ({ user }) => {
     return (
@@ -11,7 +13,12 @@ const Home = ({ user }) => {
         <div>
             <NavBar />
             {/*<h1>Welcome, {user.email}</h1>*/}
-            <NavBarLeft />
+            <div className={"page-container"}>
+                <NavBarLeft />
+                <div className={"file-view"}>
+                    <FileQueryBar />
+                </div>
+            </div>
         </div>
     );
 };
