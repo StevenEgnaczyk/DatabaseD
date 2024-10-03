50591b57b0d782ded1161b536983d446a3fa4a18
Adam3Haikal3 <haikal.8@osu.edu>
2024-10-03 19:12:02 -0400

    Fixed signup.2

--- src/components/Startup/Signup.jsx
+++ src/components/Startup/Signup.jsx
@@ -50,18 +50,15 @@ const Signup = ({ setUser }) => {
       setLoading(false); // Reset loading state
     }
   }
+  
+  return (
+    <form className={"form-container"} onSubmit={handleSignup}>
+      <h2>Signup</h2>
+      <input className={"custom-input"} type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required />
+      <input className={"custom-input"} type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
+      <button className={"submit-button"} type="submit" disabled={loading}>Signup</button>
+    </form>
+  );
 };
-
-return (
-
-  <form className={"form-container"} onSubmit={handleSignup}>
-    <h2>Signup</h2>
-    <input className={"custom-input"} type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required />
-    <input className={"custom-input"} type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
-    <button className={"submit-button"} type="submit" disabled={loading}>Signup</button>
-  </form>
-
-
-);
 
 export default Signup;
