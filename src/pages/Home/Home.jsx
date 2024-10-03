45af50044cb7532dd0b91a7e1a0c7642c2400461
Kellen <kellen.gary@gmail.com>
2024-10-03 13:16:50 -0400

    Added an upload file button within a new component called NavBarLeft

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,13 +3,15 @@
 import React from 'react';
 import "./Home.css"
 import NavBar from './../../components/Home/NavBar';
+import NavBarLeft from './../../components/Home/NavBarLeft';
 
 const Home = ({ user }) => {
     return (
 
         <div>
             <NavBar />
-            <h1>Welcome, {user.email}</h1>
+            {/*<h1>Welcome, {user.email}</h1>*/}
+            <NavBarLeft />
         </div>
     );
 };
