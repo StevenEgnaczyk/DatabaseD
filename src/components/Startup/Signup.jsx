fef29fc04deed13749f82146271c36cb80a6eeff
Adam3Haikal3 <haikal.8@osu.edu>
2024-10-02 21:23:35 -0400

    fixed signup behavior

--- src/components/Startup/Signup.jsx
+++ src/components/Startup/Signup.jsx
@@ -34,7 +34,6 @@ const Signup = ({ setUser }) => {
       <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required />
       <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
       <button type="submit">Signup</button>
-      {error && alert(error)}
     </form>
   );
 };
