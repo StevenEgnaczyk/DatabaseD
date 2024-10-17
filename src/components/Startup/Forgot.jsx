3a764691c7e39200e388a6f733f14257d58fecc7
Nginaldi22 <nginaldi@gmail.com>
2024-10-17 16:19:43 -0400

    started forgot password

--- src/components/Startup/Forgot.jsx
+++ src/components/Startup/Forgot.jsx
@@ -25,11 +25,10 @@ const Login = ({ setUser }) => {
 
   return (
       <form className={"form-container"} onSubmit={handleLogin}>
-        <h2>Log-in</h2>
+        <h2>Test</h2>
         <input className={"custom-input"} type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required />
         <input className={"custom-input"} type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
         <button className={"submit-button"} type="submit">Login</button>
-        <p><a href='src\components\Startup\Forgot.jsx'>Forgot Password?</a></p>
       </form>
   );
 };
